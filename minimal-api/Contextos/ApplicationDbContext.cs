050d29765a448e4bb4f7468b2dc02ef41e0b7c3c
Stefano <stefanolucagaggero@gmail.com>
2026-06-12 19:07:05 -0300

    Agregar archivos de proyecto.

---
+++ minimal-api/Contextos/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using minimal_api.Entidades;
+
+namespace minimal_api.Contextos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Persona> Personas { get; set; }
+
+    }
+}
---
+++ minimal-api/Entidades/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace minimal_api.Entidades
+{
+    public class Persona
+    {
+
+        public int Id { get; set; }
+
+        public required string Nombre { get; set; } 
+    }
+}
